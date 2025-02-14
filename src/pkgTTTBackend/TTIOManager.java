49eed1f4d6065ed673690ddf4fa67b70547863e1
Tuan Ton <tonthattuanst@gmail.com>
2025-02-13 21:45:59 -0800

    rowColPromp and quit and boardComplete

--- src/pkgTTTBackend/TTIOManager.java
+++ src/pkgTTTBackend/TTIOManager.java
@@ -12,4 +12,18 @@ public class TTIOManager {
     public void cellNotFreeMessage(int row, int col){
         System.out.println("Cell [" + row + "][" + col + "] is not free. Try again.");
     }
+
+    public void rowColPrompt(){
+        System.out.print("Enter row and column numbers (0 2): ");
+    }
+
+    public boolean readQuitInput(){
+        System.out.print("Do you want to quit? Enter q: ");
+        String input = myScanner.next();
+        return input.equals("q");
+    }
+
+    public void boardCompleteMessage(){
+        System.out.println("Board is full. Game over.");
+    }
 }
