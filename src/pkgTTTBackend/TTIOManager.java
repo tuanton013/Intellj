f93e416c80d9adb8c9da1fe66c937d59d81b590d
Tuan Ton <tonthattuanst@gmail.com>
2025-02-13 21:48:15 -0800

    printBoard function

--- src/pkgTTTBackend/TTIOManager.java
+++ src/pkgTTTBackend/TTIOManager.java
@@ -39,4 +39,18 @@ public class TTIOManager {
         }
         return input;
     }
+
+    public void printBoard(TTTTTBoard board){
+        char[][] boardArray = board.getBoard();
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(boardArray[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
 }
