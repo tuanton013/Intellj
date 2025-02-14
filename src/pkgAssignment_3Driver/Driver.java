4b9d956e6e1ad5ccb3ff35a749e4e693e11bd8f2
Tuan Ton <tonthattuanst@gmail.com>
2025-02-13 21:21:18 -0800

    created variables in Board function and template function

--- src/pkgAssignment_3Driver/Driver.java
+++ src/pkgAssignment_3Driver/Driver.java
@@ -6,12 +6,12 @@ public class Driver {
     public static void main(String[] args) {
         pkgTTTBackend.TTTTTBoard my_board = new pkgTTTBackend.TTTTTBoard();
 
-//        initPrompt();
-//        printBoard(my_board);
-//        my_board.testPlay();
+        initPrompt();
+        printBoard(my_board);
+        my_board.testPlay();
         System.out.println("\nStart interactive play");
-//        my_board.clearBoard();
-//        my_board.play();
+        my_board.clearBoard();
+        my_board.play();
     }
 }  // public class Driver
 
--- src/pkgTTTBackend/TTTTTBoard.java
+++ src/pkgTTTBackend/TTTTTBoard.java
@@ -1,4 +1,29 @@
 package pkgTTTBackend;
 
 public class TTTTTBoard {
+    private char machine_char = 'M';
+    private final int ROW = 3;
+    private final int COL = 3;
+    private int totalValidentries;
+    private char winner_char;
+    private char defualt_char = ' ';
+    private char[][] ttt_board = new char[ROW][COL];
+    private char player_char = 'P';
+
+    public char[][] getBoard() {
+        return ttt_board;
+    }
+
+    public void testPlay() {
+
+    }
+
+    public void clearBoard() {
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                ttt_board[i][j] = defualt_char;
+            }
+        }
+    }
+
 }
