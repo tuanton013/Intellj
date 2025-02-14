58899c11bb09c1b0fcf5b1fa411ea74a9e35fbca
Tuan Ton <tonthattuanst@gmail.com>
2025-02-13 20:53:55 -0800

    updated Driver.java

---
+++ src/pkgAssignment_3Driver/Driver.java
@@ -0,0 +1,18 @@
+package pkgAssignment_3Driver;
+
+import static pkgTTTBackend.SlIOManager.*;
+
+public class Driver {
+    public static void main(String[] args) {
+        pkgTTTBackend.TTTTTBoard my_board = new pkgTTTBackend.TTTTTBoard();
+
+        initPrompt();
+        printBoard(my_board);
+        my_board.testPlay();
+        System.out.println("\nStart interactive play");
+        my_board.clearBoard();
+        my_board.play();
+    }
+}  // public class Driver
+
+
