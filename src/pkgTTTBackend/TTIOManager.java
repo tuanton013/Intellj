5076007c6cebd0a3324f19d1dabd84cb552fdf34
Tuan Ton <tonthattuanst@gmail.com>
2025-02-13 21:44:26 -0800

    TTIO class constructor and cell is full function

--- src/pkgTTTBackend/TTIOManager.java
+++ src/pkgTTTBackend/TTIOManager.java
@@ -1,4 +1,15 @@
 package pkgTTTBackend;
 
+import java.util.Scanner;
+
 public class TTIOManager {
+    private TTIOManager(){
+
+    }
+
+    private Scanner myScanner = new Scanner(System.in);
+
+    public void cellNotFreeMessage(int row, int col){
+        System.out.println("Cell [" + row + "][" + col + "] is not free. Try again.");
+    }
 }
